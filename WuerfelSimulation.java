ec08f7642b1ed99ef40e084d435c94e226d383d9
HerbertWenisch <herbert.wenisch@t-online.de>
2021-04-09 10:37:00 +0200

    Initial sharing of project

---
+++ WuerfelSimulation.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+
+public class WuerfelSimulation {
+    private int N = 100;
+    private Random zG;
+    private int [] zzs;
+    
+    public WuerfelSimulation(){
+        zG = new Random();
+        zzs = new int[N];
+        wuerfeln();
+    }
+    
+    // würfelt einmal:
+    private int wuerfle(){
+        return zG.nextInt(6) + 1;
+    }
+    
+    // würfelt N-mal:
+    public void wuerfeln(){
+        for (int i = 0; i < N; i++) 
+             zzs[i] = wuerfle();
+    }    
+    
+    // wie oft kommt z vor?
+    public int getNumbersOf(int z){
+        int count = 0;  // Zähler
+        for(int i = 0; i < N; i++)
+            if(zzs[i] == z) count++;
+        return count;    
+    } 
+    
+}
